3bc35ff2e712715de9ea07c3a14078a46544a632
Latravia Gordon <latravia.gordon@gmail.com>
2022-01-19 01:16:05 -0500

    added function and alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// window.alert("This is an alert! JavaScript is running!");
+
+
+
+var playerName = window.prompt("What is your robot's name?");
+//prompt//user input
+//var is used to capture the input
+//window.alert(playerName); //contains robot name when entered
+console.log(playerName); //use instead to test code w/o interrupting the page
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+//this creates a function named "fight
+function fight() { //declare
+    window.alert("The fight has begun!");
+}
+// fight(); //call
